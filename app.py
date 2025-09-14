c007e0e332f3d84a95b75a45f9ff6b0577917458
Rakib5032 <rbhuiyan027@gmail.com>
2025-09-14 22:07:31 +0600

    Updated

--- app.py
+++ app.py
@@ -165,8 +165,6 @@ def api_predict():
 
 
 if __name__ == '__main__':
-    os.makedirs('models', exist_ok=True)
-    os.makedirs('templates', exist_ok=True)
-    os.makedirs('static', exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
